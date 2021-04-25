84a97e31a5730784bba1919a7520247e0b9e12ae
Nikita Belevich <belevish_1998@mail.ru>
2021-04-25 08:15:04 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const myMath = require('./modules/math');
+const fs = require('fs');
+const util = require('util');
+const EventEmitter = require('events');
+const http = require('http');
+
+
+const url = require('url');
+
+const hostname = '127.0.0.5';
+const port = 3005;
+const server = http.createServer((request, response) => {
+    
+    
+
+});
+
